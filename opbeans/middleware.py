36ab87f5b3103e626b4a879ee4fc2a5ea1a64eda
Benjamin Wohlwend <beni@elastic.co>
2018-02-08 18:01:52 +0100

    set the user to some fake log in if it isn't logged in already
    closes #6

--- opbeans/middleware.py
+++ opbeans/middleware.py
@@ -1,11 +1,33 @@
 import uuid
+import random
+from collections import namedtuple
 
 import elasticapm
 
 
+User = namedtuple('User', ['is_authenticated', 'id', 'username', 'email'])
+
+users = [
+    User(True, 1, 'arthurdent', 'arthur.dent@example.com'),
+    User(True, 2, 'fprefect', 'ford.prefect@example.com'),
+    User(True, 3, 'trillian', 'adastra@example.com'),
+    User(True, 4, 'zaphod', 'z@example.com'),
+]
+
+
 def tag_request_id_middleware(get_response):
     def middleware(request):
+        __traceback_hide__ = True
         elasticapm.tag(request_id=str(uuid.uuid4()))
         response = get_response(request)
         return response
+    return middleware
+
+
+def user_middleware(get_response):
+    def middleware(request):
+        __traceback_hide__ = True
+        if not request.user.is_authenticated:
+            request.user = random.choice(users)
+        return get_response(request)
     return middleware
--- opbeans/settings.py
+++ opbeans/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'opbeans.middleware.user_middleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
