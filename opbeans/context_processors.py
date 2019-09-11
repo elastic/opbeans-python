958439e84d96b5af1dc9b905145ed111030a7077
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-09-11 12:43:22 +0200

    ci(jenkins): update docker-compose files

--- opbeans/context_processors.py
+++ opbeans/context_processors.py
@@ -11,14 +11,14 @@ def rum_settings(request):
     global RUM_CONFIG
     if RUM_CONFIG:
         return RUM_CONFIG
-    url = os.environ.get('ELASTIC_APM_JS_BASE_SERVER_URL', os.environ.get('ELASTIC_APM_JS_SERVER_URL'))
+    url = os.environ.get('ELASTIC_APM_JS_SERVER_URL')
     if not url:
         app = apps.get_app_config('elasticapm.contrib.django')
         url = app.client.config.server_url
     with open(os.path.join(settings.BASE_DIR, 'opbeans', 'static', 'package.json')) as f:
         package_json = json.load(f)
-    service_name = os.environ.get('ELASTIC_APM_JS_BASE_SERVICE_NAME', package_json['name'])
-    service_version = os.environ.get('ELASTIC_APM_JS_BASE_SERVICE_VERSION', package_json['version'])
+    service_name = os.environ.get('ELASTIC_APM_JS_SERVICE_NAME', package_json['name'])
+    service_version = os.environ.get('ELASTIC_APM_JS_SERVICE_VERSION', package_json['version'])
     RUM_CONFIG = {
         "RUM_SERVICE_NAME": service_name,
         "RUM_SERVICE_VERSION": service_version,
