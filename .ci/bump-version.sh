63c87118dd931fdcf93bc7a6a26fb36b69546782
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-07-01 12:56:57 +0100

    docker: add labels metadata

--- .ci/bump-version.sh
+++ .ci/bump-version.sh
@@ -5,6 +5,9 @@ AGENT_VERSION="${1?Missing the APM python agent version}"
 
 sed -ibck "s#elastic-apm==.*#elastic-apm==${AGENT_VERSION}#g" requirements.txt
 
+## Bump agent version in the Dockerfile
+sed -ibck "s#\(org.label-schema.version=\)\(.*\)#\1\"${AGENT_VERSION}\"#g" Dockerfile
+
 # Commit changes
-git add requirements.txt
+git add requirements.txt Dockerfile
 git commit -m "Bump version ${AGENT_VERSION}"
