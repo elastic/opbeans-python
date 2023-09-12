ae5f4fff4d1ec1cab1f26b845d3b006b1099c24b
Colton Myers <colton.myers@gmail.com>
2023-09-12 09:31:50 -0600

    Upgrade elasticsearch deps, now that elasticsearch-dsl is updated (#272)
    * Upgrade elasticsearch deps, now that elasticsearch-dsl is updated
    
    * Gate the elasticsearch create_connection

--- opbeans/celery.py
+++ opbeans/celery.py
@@ -1,20 +1,24 @@
 from __future__ import absolute_import
+
 import os
+
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opbeans.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opbeans.settings")
 
 import django
+
 django.setup()
 
-app = Celery('opbeans')
+app = Celery("opbeans")
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
 
 
-from elasticsearch_dsl.connections import connections
 from django.conf import settings
+from elasticsearch_dsl.connections import connections
 
-connections.create_connection(hosts=settings.ELASTICSEARCH_URL)
+if settings.ELASTICSEARCH_URL:
+    connections.create_connection(hosts=settings.ELASTICSEARCH_URL)
